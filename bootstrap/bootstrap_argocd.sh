fe73efb765bfdc7266bbf82af43ece1d11a305f9
alexneuman <alexshrugged92@gmail.com>
2025-09-19 14:17:32 -0700

    no-message

--- bootstrap/bootstrap_argocd.sh
+++ bootstrap/bootstrap_argocd.sh
@@ -15,6 +15,7 @@ done
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 30
 kubectl apply -f k8s/argocd/${ENV}/ --recursive
 
 echo "Bootstrap Password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
