0ac43ed85e3f205d68da52a737ea55991f686b7e
alexneuman <alexshrugged92@gmail.com>
2025-09-27 10:23:20 -0700

    redis-commander ingress

--- bootstrap/bootstrap_argocd.sh
+++ bootstrap/bootstrap_argocd.sh
@@ -15,7 +15,8 @@ done
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sleep 30
+sleep 35
 kubectl apply -f k8s/argocd/${ENV}/ --recursive
 
 echo "Bootstrap Password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+echo "port-forward cmd: kubectl port-forward svc/argocd-server -n argocd 8085:80"
