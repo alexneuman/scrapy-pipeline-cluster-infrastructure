32e5139dcefd63b84f0e84181a8be412dcdc8a2f
alexneuman <alexshrugged92@gmail.com>
2025-10-07 22:11:19 -0700

    no-msg

--- bootstrap/bootstrap_argocd.sh
+++ bootstrap/bootstrap_argocd.sh
@@ -18,5 +18,10 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 sleep 35
 kubectl apply -f k8s/argocd/${ENV}/ --recursive
 
+# Patch argocd-server to enable HTTP-only if dev
+if [[ "$ENV" == "dev" ]]; then
+  kubectl -n argocd patch deployment argocd-server --type='json' -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--insecure"}]'
+fi
+
 echo "Bootstrap Password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
-echo "port-forward cmd: kubectl port-forward svc/argocd-server -n argocd 8085:80"
+echo "port-forward cmd: kubectl port-forward svc/ingress-nginx-controller -n ingress-nginx 8085:80"
